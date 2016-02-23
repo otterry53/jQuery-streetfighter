2f559012fadf5cec87dfe022c4400e5fd8dc0fcb
otterry53 <twlow88@gmail.com>
2016-02-23 14:10:13 -0500

    add audio

--- js/app.js
+++ js/app.js
@@ -4,18 +4,34 @@ $(document).ready(function(){
     $('.ryu-ready').show();  
   })
    .mouseleave(function() {
-   	 $('.ryu-ready').hide();
    	 $('.ryu-still').show();
+   	 $('.ryu-ready').hide();
+   	 
    })
     .mousedown(function() {
-       // play hadouken sound
+       playHadouken();
       $('.ryu-ready').hide()
       $('.ryu-throwing').show();
-      $('.hadouken').show();
-       // animate hadouken to the right of the screen
+      $('.hadouken').finish().show().animate(
+      	  {'left': '1020px'},
+      	  500,
+      	  function() {
+      	  	$(this).hide();
+      	  	$(this).css('left', '520px');
+      	  });
     })
     .mouseup(function() {
       $('.ryu-throwing').hide();
       $('.ryu-ready').show();
     });
 });
+
+    function playHadouken() {
+    	$('#hadouken-sound')[0].volume = 0.5;
+    	$('#hadouken-sound')[0].load();
+		$('#hadouken-sound')[0].play();
+    }
+
+
+
+
